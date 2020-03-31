1554c72d06c09b0695740c581e8cd1918369fd9a
Tom Banish <tombanish@gmail.com>
2020-03-31 23:24:33 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,6 +7,10 @@ class Backer
     @backed_projects = []
   end
   
+  def add_project(project)
+    @backed_projects << project
+  end
+  
   def back_project(project)
     @backed_projects << project
     project.add_backer(self)
