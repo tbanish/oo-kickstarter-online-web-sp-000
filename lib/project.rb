530db10decf58a1f4b7155dfd7cdb9db291e70ff
Tom Banish <tombanish@gmail.com>
2020-03-31 23:06:45 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+class Project
+  attr_accessor :backers
+  attr_reader :title
+  
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+  
+  def add_backer(backer)
+    #binding.pry
+    @backers << backer
+
+    
+  end
+  
+end
